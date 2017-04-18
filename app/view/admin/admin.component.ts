293b24710b6af2109046cd60f85c04aa46ed74f1
wolodymyr-doc <wolodymyr.doc@gmail.com>
2017-04-18 19:31:36 +0300

    Extra node

--- app/view/admin/admin.component.ts
+++ app/view/admin/admin.component.ts
@@ -9,6 +9,22 @@ import {Component, OnInit, Input, Output, EventEmitter} from '@angular/core';
 })
 
 export class AdminComponent {
+   categories: Categories[] = [
+   {
+     name1: 'Books',
+     name2: 'Книги',
+     id: 1,
+     level: 0,
+     parent: 0
+   },
+   {
+     name1: 'Video',
+     name2: 'Відео',
+     id: 2
+   }
+  ] as Categories[];
+
+
 
     constructor() {
     }
