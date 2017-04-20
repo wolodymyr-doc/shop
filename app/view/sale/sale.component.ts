3a0f403878d9292a1180c555c12d77051a182ab3
AndreyDovbash <andjmail1@gmail.com>
2017-04-20 19:36:08 +0300

    Add files via upload

---
+++ app/view/sale/sale.component.ts
@@ -0,0 +1,14 @@
+import {Component, OnInit, Input, Output, EventEmitter} from '@angular/core';
+      
+@Component({
+    selector: 'my-app',
+    templateUrl: 'app/view/sale/sale.components.html',
+    styleUrls: ['app/view/sale/sale.components.css']	
+   
+})
+export class SaleComponent {
+
+    constructor() {
+    }
+
+}
