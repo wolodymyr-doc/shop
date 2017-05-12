72a3efd8a5d3168b2c465b7844908949f789ca70
AndreyDovbash <andjmail1@gmail.com>
2017-05-12 20:21:30 +0300

    Add files via upload

--- app/view/sale/menu0/menu.component.ts
+++ app/view/sale/menu0/menu.component.ts
@@ -3,6 +3,6 @@ import {Component} from '@angular/core';
 
 @Component({
   selector: 'menu-0',
-  template: '<button md-button [mdMenuTriggerFor]="menu">Menu</button><md-menu #menu="mdMenu"><button md-menu-item>Item 1</button><button md-menu-item>Item 2</button></md-menu>'
+  templateUrl: './menu.component.html',
 })
 export class SaleMenuComponent {}
